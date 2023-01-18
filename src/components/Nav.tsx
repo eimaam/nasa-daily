eadd31b36296e380aa28bcf6be0ea13a9e5b9193
eimaam <imamddahir@gmail.com>
2023-01-18 12:20:36 +0100

    chore: changed Link to anchor tag for menu items with links

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,15 +1,22 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Nav = () => {
   return (
     <nav>
-        <ul>
-            <li>Home</li>
-            <li>API Doc</li>
-            <li>TODAY</li>
-            <li>About</li>
-            <li>Source Code</li>
-        </ul>
+      <div>
+        <h2>eimaam</h2>
+      </div>
+      <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><a href="https://api.nasa.gov/index.html#apply-for-an-api-key">API Doc</a></li>
+          <li><Link to="/photooftheday">TODAY</Link></li>
+          <li><Link to="">About</Link></li>
+          <li><a href="https://www.github.com/eimaam/nasa-daily">Source Code</a></li>
+      </ul>
+      <div>
+        <button>logOut</button>
+      </div>
     </nav>
   )
 }
