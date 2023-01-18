44566c980ee199640c843f97b1e43d1a42d941e5
eimaam <imamddahir@gmail.com>
2023-01-18 09:21:04 +0100

    feat: initiated data fetch from API

--- src/components/ImageToday.tsx
+++ src/components/ImageToday.tsx
@@ -2,11 +2,40 @@ import React, { useEffect, useState } from 'react'
 
 
 export const ImageToday = () => {
-   
+    interface ImageProps{
+        date: string,
+        title: string,
+        details: string,
+        url: string,
+    }
+
+    const [photoDetails, setPhotoDetails] = useState<ImageProps>({
+        date: "",
+        title: "",
+        details: "",
+        url: "",
+    });
+
+    useEffect(() => {
+        const fetchImage = async () => {
+            try{
+                fetch(`https://api.nasa.gov/planetary/apod?api_key=XVSgPag6qsDHs5MyMoYgEl7DedgA078UG8Qtr0Yw`)
+                .then(res => res.json())
+                .then(data => setPhotoDetails(data))
+            }
+            catch(err:any){
+                console.log(err.message)
+            }
+        }
+
+        fetchImage()
+
+    }, [])
+
 
   return (
     <div>
-        
+        <img src={photoDetails.url} alt="" />
     </div>
   )
 }
