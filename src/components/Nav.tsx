9ed13d7880e8e066b987069b79855e32c9d13d83
eimaam <imamddahir@gmail.com>
2023-01-18 15:14:24 +0100

    feat: added mobile styling and nav toggler

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { GoThreeBars } from "react-icons/go"
 
 export const Nav = () => {
+  const toggleNav = () => {
+    const navItems = document.getElementById('navItems') as any;
+
+    if(navItems.style.display != "flex"){
+      navItems.style.display = "flex"
+    }else{
+      navItems.style.display = "none"
+    }
+
+  }
+
   return (
     <nav>
       <div>
-        <h2>eimaam</h2>
+        <h2>@eimaam</h2>
       </div>
-      <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><a href="https://api.nasa.gov/index.html#apply-for-an-api-key">API Doc</a></li>
-          <li><Link to="/photooftheday">TODAY</Link></li>
-          <li><Link to="">About</Link></li>
-          <li><a href="https://www.github.com/eimaam/nasa-daily">Source Code</a></li>
+      <ul id='navItems'>
+        <li><Link to="/">Home</Link></li>
+        <li><a href="https://api.nasa.gov/index.html#apply-for-an-api-key">API Doc</a></li>
+        <li><Link to="/photooftheday">TODAY</Link></li>
+        <li><Link to="">About</Link></li>
+        <li><a href="https://www.github.com/eimaam/nasa-daily">Source Code</a></li>
+        <li id='logOutMobile'>
+          <button type='button'>logOut</button>
+        </li>
       </ul>
-      <div>
+      <div id='logOut'>
         <button>logOut</button>
+      </div>
+      <div id="toggler" onClick={toggleNav}>
+        <h1><GoThreeBars /></h1>
       </div>
     </nav>
   )
