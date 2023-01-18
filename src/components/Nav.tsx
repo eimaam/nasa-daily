0733e707c5eaa9c5e0d3fc407f14a1e037213169
eimaam <imamddahir@gmail.com>
2023-01-18 09:12:11 +0100

    feat: added Nav Bar component

---
+++ src/components/Nav.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Nav = () => {
+  return (
+    <nav>
+        <ul>
+            <li>Home</li>
+            <li>API Doc</li>
+            <li>TODAY</li>
+            <li>About</li>
+            <li>Source Code</li>
+        </ul>
+    </nav>
+  )
+}
