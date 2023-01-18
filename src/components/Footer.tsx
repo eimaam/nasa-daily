bb0844e971eddffa4fc1e6492ac63a37b2946d0f
eimaam <imamddahir@gmail.com>
2023-01-18 12:28:13 +0100

    styling: mobile styling added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,20 +2,28 @@ import React from 'react'
 
 export const Footer = () => {
   return (
-    <div>
+    <footer>
+      <div className='footer--items'>
         <div>
             <h2>NASA DAILY</h2>
         </div>
-        <ul>
-            <li>Privacy Doc.</li>
-            <li>NASA</li>
-            <li>Officials</li>
-            <li>API Documentation</li>
-        </ul>
+        <div className='mid'>
+          <ul>
+              <li>Privacy Doc.</li>
+              <li>NASA</li>
+              <li>Officials</li>
+              <li>API Documentation</li>
+          </ul>
+          <div>
+            <h3>Contact:</h3>
+            <p>support@nasadaily.org</p>
+          </div>
+        </div>
         <div>
             <p>All rights reserved.</p>
             <p>&copy; MYM 2023.</p>
         </div>
-    </div>
+      </div>
+    </footer>
   )
 }
