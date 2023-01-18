1ebfb0571fc6aa3cb24c803fafda9cd65fd9f704
eimaam <imamddahir@gmail.com>
2023-01-18 12:19:51 +0100

    initiated page Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import { ImageToday } from './components/ImageToday';
+import { ImageToday } from './components/contentPage/ImageToday';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from './components/Home';
+import { Nav } from './components/Nav';
 
 
 function App() {
   return (
-    <>
-      <ImageToday />
-    </>
+    <BrowserRouter>
+    <Nav />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='photooftheday' element={<ImageToday />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
