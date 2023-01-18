6bdc8b8f7107dab0698b9cf09c84a92fe90a235e
eimaam <imamddahir@gmail.com>
2023-01-18 09:12:23 +0100

    feat: added footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+        <div>
+            <h2>NASA DAILY</h2>
+        </div>
+        <ul>
+            <li>Privacy Doc.</li>
+            <li>NASA</li>
+            <li>Officials</li>
+            <li>API Documentation</li>
+        </ul>
+        <div>
+            <p>All rights reserved.</p>
+            <p>&copy; MYM 2023.</p>
+        </div>
+    </div>
+  )
+}
