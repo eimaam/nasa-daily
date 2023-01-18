911e305cf0dd43cd2c0dc8891ff7130d9d217172
eimaam <imamddahir@gmail.com>
2023-01-18 15:21:01 +0100

    docs: changed position of date

--- src/components/contentPage/Header.tsx
+++ src/components/contentPage/Header.tsx
@@ -3,8 +3,9 @@ import React from 'react'
 export const Header = ({ date }:{date: string}) => {
   return (
     <header>
-        <h2>{date}</h2>
         <h1>~ NASA ~</h1>
+        <br />
+        <h2>{date}</h2>
         <h2>Image of the Day:</h2>
     </header>
   )
