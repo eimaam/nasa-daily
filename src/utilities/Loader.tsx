8fe22f9ae1988d9f0d661fb0179eb75e3d877202
eimaam <imamddahir@gmail.com>
2023-01-19 20:25:26 +0100

    feat: created loader component

---
+++ src/utilities/Loader.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { MoonLoader } from 'react-spinners';
+
+export const Loader = () => {
+
+  return (
+    <div className='loader'>
+        <MoonLoader />
+    </div>
+  )
+}
